542b113fd018e959073e983c88d975abbd1b28e8
foesublime <rylandcs@gmail.com>
2022-12-24 11:05:02 -0700

    added html and java.index with definitions to current work

---
+++ index.js
@@ -0,0 +1,34 @@
+// JavaScript Comparison Operator
+// Creates executable code
+
+
+//  == equal to
+//  != not equal to
+//  > Greater than
+//  < Less than 
+//  >= greater than or equal to
+//  <= less than or equal to
+//  === checks value and type is same as == and !=
+
+var currentAccountBalance = 74.98;
+var purchase = 74.99;
+var freeLavalampVoucher = true
+
+
+if (purchase <= currentAccountBalance) {
+  console.log('Lava lamps for days');
+
+// else if adds conditional variables 
+} else if (freeLavalampVoucher) {
+  console.log('Freebiesssss');
+}
+
+else {
+  console.log('Broke...');
+}
+
+
+
+
+
+//  console.log();
